4b715ee36e8debc55d7b5d7f791701f23bde5b41
Tom Corvus <tom.corvus@live.fr>
2020-07-19 17:51:12 +0200

    Correction du test avec Jest

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,10 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 jest.useFakeTimers();
 
-it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
+test('renders correctly', async () => {
+	const tree = renderer.create(<App />).toJSON();
+	expect(tree).toMatchSnapshot();
 });
